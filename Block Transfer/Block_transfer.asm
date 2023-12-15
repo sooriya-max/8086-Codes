96b457e71bba7e91dafbc25c616e7c2f1b3454c8
Sooriya S <53864727+sooriya-max@users.noreply.github.com>
2023-12-15 22:47:13 +0530

    Update Block_transfer.asm
    Updated Comments

--- Block Transfer/Block_transfer.asm
+++ Block Transfer/Block_transfer.asm
@@ -2,22 +2,22 @@ CODE SEGMENT
     ASSUME CS:CODE
 START:
     MOV AX, 2000H
-    MOV DS, AX
+    MOV DS, AX      ;This Initializes the DS for Source
     
     MOV AX, 3000H
-    MOV ES, AX
+    MOV ES, AX      ;This Intializes the ES for Destination
     
-    MOV SI, 5000H
-    MOV DI, 6000H
+    MOV SI, 5000H   ;Source pointer or offset address for DS
+    MOV DI, 6000H   ;Destination pointer or offset address for ES
     
-    MOV CX, 0400H
+    MOV CX, 0400H   ;Count
     
-    CLD
+    CLD             ;Clear the Direction Flag
     
-    REP MOVSB
+    REP MOVSB       ;Repeat Move String Byte
     
     MOV AH, 04H
-    INT 21H
+    INT 21H         ;Program Halt
 
 CODE ENDS
      END START
