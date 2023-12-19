5b1be03a6752200ce86a827a2411610768c1ae66
Sooriya S <53864727+sooriya-max@users.noreply.github.com>
2023-12-19 20:36:52 +0530

    Update Block_transfer_bw_2_mem.asm

--- Block_Transfer_bw_2_mem/Block_transfer_bw_2_mem.asm
+++ Block_Transfer_bw_2_mem/Block_transfer_bw_2_mem.asm
@@ -1,52 +1,68 @@
 CODE SEGMENT
     ASSUME CS:CODE
 START:
+    ; Initialize data segment DS with 2000H
     MOV AX, 2000H
     MOV DS, AX
     
+    ; Initialize extra segment ES with 4000H
     MOV AX, 4000H
     MOV ES, AX
     
+    ; Set source index SI to 5000H and destination index DI to 8000H
     MOV SI, 5000H
     MOV DI, 8000H
     
+    ; Set the counter CX to the number of elements to copy (0AH)
     MOV CX, 0AH
     
+    ; Clear direction flag to increment SI and DI after each byte transfer
     CLD
     
+    ; Repeat block transfer of CX bytes from DS:SI to ES:DI
     REP MOVSB
     
-    
+    ; Move to the next data segment
     MOV AX, 3000H
     MOV DS, AX
     
+    ; Move to the next extra segment
     MOV AX, 2000H
     MOV ES, AX
     
+    ; Set source index SI to 6000H and destination index DI to 5000H
     MOV SI, 6000H
     MOV DI, 5000H
     
+    ; Set the counter CX to the number of elements to copy (0AH)
     MOV CX, 0AH
     
+    ; Clear direction flag to increment SI and DI after each byte transfer
     CLD
     
+    ; Repeat block transfer of CX bytes from DS:SI to ES:DI
     REP MOVSB
     
-    
+    ; Move to the next data segment
     MOV AX, 4000H
     MOV DS, AX
     
+    ; Move to the next extra segment
     MOV AX, 3000H
     MOV ES, AX
     
+    ; Set source index SI to 8000H and destination index DI to 6000H
     MOV SI, 8000H
     MOV DI, 6000H
     
+    ; Set the counter CX to the number of elements to copy (0AH)
     MOV CX, 0AH
     
+    ; Clear direction flag to increment SI and DI after each byte transfer
     CLD
     
+    ; Repeat block transfer of CX bytes from DS:SI to ES:DI
     REP MOVSB
     
 CODE ENDS
-     END START
+    END START
